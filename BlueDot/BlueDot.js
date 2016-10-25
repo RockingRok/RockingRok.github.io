0178e71ccc08dab52fa62cabb47074f2ca4cfdc0
RockingRok <nickshlapkov@gmail.com>
2016-10-24 21:04:09 -0500

    Readability and Abstraction
    Renamed some functions, removed useless variables, fixed issue with
    clicking (clicking too fast would cause animation glitches).

--- BlueDot/BlueDot.js
+++ BlueDot/BlueDot.js
@@ -37,8 +37,6 @@ var c=document.getElementById("myCanvas");
 var ctx=c.getContext("2d");
 ctx.font = "30px Arial";
 var animation_flag = 0;
-var end_animation_flag = 0;
-var random_movement_flag = 0;
 var victory_flag = 0;
 var blue_dot = new Point(0, 0, (new Point(0, 0, null)));
 var score = 0;
@@ -54,8 +52,6 @@ var game_state = [[0, 0, 0, 0, 0, 0, 0, 0, 0],
 
 function reset_game_state() {
     animation_flag = 0;
-    end_animation_flag = 0;
-    random_movement_flag = 0;
     victory_flag = 0;
     blue_dot = new Point(0, 0, (new Point(0, 0, null)));
     score = 0;
@@ -167,15 +163,9 @@ window.onload = function () {
 }
 
 function animation() {
-    if (random_movement_flag == 0) {
-        display_game_state();
-        game_state[blue_dot.row][blue_dot.column] = 2;
-    }
-    else {
-        game_state[blue_dot.row][blue_dot.column] = 0;
-        display_game_state();
-        game_state[blue_dot.row][blue_dot.column] = 2;
-    }
+    game_state[blue_dot.row][blue_dot.column] = 0;
+    display_game_state();
+    game_state[blue_dot.row][blue_dot.column] = 2;
     if ((blue_dot.parent).row % 2 == 0) //no offset
     {
         if (blue_dot.row == ((blue_dot.parent).row - 1) && blue_dot.column == (blue_dot.parent).column) //top right
@@ -232,26 +222,34 @@ function animation() {
     }
 }
 
-function UpdateScore() {
+function display_score() {
     ctx.fillText("Score: " + score, 800, 50);
 }
 
-function ClearScreen() {
+function clear_screen(display_flag, increment_flag) {
     ctx.clearRect(0, 0, 1000, 1000);
     display_game_state();
     ctx.drawImage(document.getElementById("refresh"), 800, 600);
+    if (increment_flag)
+    {
+        score++;
+    }
+    if (display_flag)
+    {
+        display_score();
+    }
 }
 
 var path = []; // (row, col), (row, col), etc.
-function bluedotmove() {
+function bluedotmove_path() {
     game_state[blue_dot.row][blue_dot.column] = 0;
     (blue_dot.parent).row = blue_dot.row;
     (blue_dot.parent).column = blue_dot.column;
     blue_dot.row = path.shift();
     blue_dot.column = path.shift();
-    var myVar1 = setTimeout(animation, 100);
-    var myVar2 = setTimeout(ClearScreen, 200);
-    var myVar3 = setTimeout(UpdateScore, 200);
+    path = path.slice(0, 0);
+    var myVar1 = setTimeout(function () { animation_flag = 1; animation(); }, 100);
+    var myVar2 = setTimeout(function () { clear_screen(1, 0); animation_flag = 0; }, 200);
 }
 
 var queue = [];
@@ -264,11 +262,12 @@ function queue_point(point, row_add, col_add)
 
 function MazeSolver(row, column) {
     queue.push(new Point(row, column, null));
-    while (queue.length != 0) //true
+    while (queue.length != 0)
     {
         var p = queue.shift();
 
         if ((p.column == 0 || p.column == 8) || (p.row == 0 || p.row == 8)) {
+            queue = queue.slice(0, 0);
             return p;
         }
 
@@ -363,21 +362,20 @@ function checkMoves() {
     }
 }
 
-function RandomMove_movement(row_add, col_add)
+function bluedotmove_rand(row_add, col_add)
 {
     game_state[blue_dot.row][blue_dot.column] = 0;
-    game_state[blue_dot.row + row_add][blue_dot.column + col_add] = 2;
+    (blue_dot.parent).row = blue_dot.row;
+    (blue_dot.parent).column = blue_dot.column;
     blue_dot.row = blue_dot.row + row_add;
     blue_dot.column = blue_dot.column + col_add;
-    var myVar1 = setTimeout(animation, 100);
-    var myVar2 = setTimeout(ClearScreen, 200);
-    var myVar3 = setTimeout(UpdateScore, 200);
+    game_state[blue_dot.row][blue_dot.column] = 2;
+    var myVar1 = setTimeout(function () { animation_flag = 1; animation(); }, 100);
+    var myVar2 = setTimeout(function () { clear_screen(1, 0); animation_flag = 0; }, 200);
 }
 
 function RandomMove() {
     checkMoves();
-    (blue_dot.parent).row = blue_dot.row;
-    (blue_dot.parent).column = blue_dot.column;
     if (availableMoves.length > 0) {
         var direction = Math.round(Math.random() * (availableMoves.length - 1));
         direction = availableMoves[direction];
@@ -386,47 +384,47 @@ function RandomMove() {
     else {
         victory_flag = 1;
         ctx.fillText("You win!", 800, 150);
-        return;
+        return 1;
     }
     if (blue_dot.row % 2 == 0) //no offset
     {
         if (direction == "topright") {
-            RandomMove_movement(-1, 0);
+            bluedotmove_rand(-1, 0);
         }
         if (direction == "right") {
-            RandomMove_movement(0, 1);
+            bluedotmove_rand(0, 1);
         }
         if (direction == "topleft") {
-            RandomMove_movement(-1, -1);
+            bluedotmove_rand(-1, -1);
         }
         if (direction == "botright") {
-            RandomMove_movement(1, 0);
+            bluedotmove_rand(1, 0);
         }
         if (direction == "left") {
-            RandomMove_movement(0, -1);
+            bluedotmove_rand(0, -1);
         }
         if (direction == "botleft") {
-            RandomMove_movement(1, -1);
+            bluedotmove_rand(1, -1);
         }
     }
     else {
         if (direction == "topright") {
-            RandomMove_movement(-1, 1);
+            bluedotmove_rand(-1, 1);
         }
         if (direction == "right") {
-            RandomMove_movement(0, 1);
+            bluedotmove_rand(0, 1);
         }
         if (direction == "topleft") {
-            RandomMove_movement(-1, 0);
+            bluedotmove_rand(-1, 0);
         }
         if (direction == "botright") {
-            RandomMove_movement(1, 1);
+            bluedotmove_rand(1, 1);
         }
         if (direction == "left") {
-            RandomMove_movement(0, -1);
+            bluedotmove_rand(0, -1);
         }
         if (direction == "botleft") {
-            RandomMove_movement(1, 0);
+            bluedotmove_rand(1, 0);
         }
     }
 }
@@ -435,8 +433,7 @@ function RandomMove() {
 function fadeOut(col, row) {
     ctx.drawImage(document.getElementById("bluecircleanimated2"), col, row);
     setTimeout(function () {
-        ClearScreen();
-        UpdateScore();
+        clear_screen(1,0);
         ctx.fillText("You lose!", 800, 150);
         animation_flag = 0;
     }, 500)
@@ -447,8 +444,7 @@ function EndAnimation() {
     if (blue_dot.row == 8) {
         ctx.drawImage(document.getElementById("bluecircleanimated"), 10 + (blue_dot.column * 80), 10 + (blue_dot.row * 80) + 40);
         setTimeout(function () {
-            ClearScreen();
-            UpdateScore();
+            clear_screen(1,0);
             fadeOut(10 + (blue_dot.column * 80), 10 + (blue_dot.row * 80) + 80);
         }, 500)
         return;
@@ -456,8 +452,7 @@ function EndAnimation() {
     if (blue_dot.row == 0) {
         ctx.drawImage(document.getElementById("bluecircleanimated"), 10 + (blue_dot.column * 80), 10 + (blue_dot.row * 80) - 40);
         setTimeout(function () {
-            ClearScreen();
-            UpdateScore();
+            clear_screen(1,0);
             fadeOut(10 + (blue_dot.column * 80), 10 + (blue_dot.row * 80) - 80);
         }, 500)
         return;
@@ -467,16 +462,14 @@ function EndAnimation() {
         if (blue_dot.column == 8) {
             ctx.drawImage(document.getElementById("bluecircleanimated"), 10 + (blue_dot.column * 80) + 40, 10 + (blue_dot.row * 80));
             setTimeout(function () {
-                ClearScreen();
-                UpdateScore();
+                clear_screen(1,0);
                 fadeOut(10 + (blue_dot.column * 80) + 80, 10 + (blue_dot.row * 80));
             }, 500)
         }
         else if (blue_dot.column == 0) {
             ctx.drawImage(document.getElementById("bluecircleanimated"), 10 + (blue_dot.column * 80) - 40, 10 + (blue_dot.row * 80));
             setTimeout(function () {
-                ClearScreen();
-                UpdateScore();
+                clear_screen(1,0);
                 fadeOut(10 + (blue_dot.column * 80) - 80, 10 + (blue_dot.row * 80));
             }, 500)
         }
@@ -485,18 +478,47 @@ function EndAnimation() {
         if (blue_dot.column == 8) {
             ctx.drawImage(document.getElementById("bluecircleanimated"), 45 + (blue_dot.column * 80) + 40, 10 + (blue_dot.row * 80));
             setTimeout(function () {
-                ClearScreen();
-                UpdateScore();
+                clear_screen(1,0);
                 fadeOut(45 + (blue_dot.column * 80) + 80, 10 + (blue_dot.row * 80));
             }, 500)
         }
         else if (blue_dot.column == 0) {
             ctx.drawImage(document.getElementById("bluecircleanimated"), 45 + (blue_dot.column * 80) - 40, 10 + (blue_dot.row * 80));
             setTimeout(function () {
-                ClearScreen();
-                UpdateScore();
+                clear_screen(1,0);
                 fadeOut(45 + (blue_dot.column * 80) - 80, 10 + (blue_dot.row * 80));
             }, 500)
+        }
+    }
+}
+
+function game_controller(j, i)
+{
+    if ((blue_dot.row == 8 || blue_dot.row == 0) || (blue_dot.column == 8 || blue_dot.column == 0)) {
+        game_state[j][i] = 1;
+        clear_screen(1, 1);
+        game_state[blue_dot.row][blue_dot.column] = 0;
+        victory_flag = -1;
+        animation_flag = 1;
+        var myVar1 = setTimeout(EndAnimation, 500);
+    }
+    else {
+        game_state[j][i] = 1;
+        clear_screen(1, 1);
+        var p = MazeSolver(blue_dot.row, blue_dot.column);
+
+        if (p == null) {
+            display_game_state();
+            RandomMove();
+        }
+        else {
+            while (p.getParent() != null) {
+                path.push(p.getColumn());
+                path.push(p.getRow());
+                p = p.getParent();
+            }
+            path = path.reverse();
+            bluedotmove_path();
         }
     }
 }
@@ -504,107 +526,29 @@ function EndAnimation() {
 function doMouseDown(event) {
     var canvas_x = event.pageX;
     var canvas_y = event.pageY;
-    for (var j = 0; j < 9; j++) {
-        for (var i = 0; i < 9; i++) {
-            if (j % 2 == 0) {
-                if ((canvas_x > 10 + (80 * i) && canvas_x < 10 + (80 * (i + 1))) && (canvas_y > 10 + (80 * j) && canvas_y < 10 + (80 * (j + 1)))) {
-                    if (game_state[j][i] != 1 && game_state[j][i] != 2) {
-                        if ((blue_dot.row == 8 || blue_dot.row == 0) || (blue_dot.column == 8 || blue_dot.column == 0)) {
-                            if (game_state[blue_dot.row][blue_dot.column] == 2) {
-                                game_state[j][i] = 1;
-                                ClearScreen();
-                                score++;
-                                UpdateScore();
-                            }
-                            game_state[blue_dot.row][blue_dot.column] = 0;
-                            if (!end_animation_flag) {
-                                end_animation_flag = 1;
-                                animation_flag = 1;
-                                var myVar1 = setTimeout(EndAnimation, 500);
-                            }
-                        }
-                        else {
-                            if (victory_flag == 0) {
-                                ClearScreen();
-                                score++;
-                                UpdateScore();
-                                game_state[j][i] = 1;
-                                queue = queue.slice(0, 0);
-                                path = path.slice(0, 0);
-                                var p = MazeSolver(blue_dot.row, blue_dot.column);
-
-                                if (p == null) {
-                                    random_movement_flag = 1;
-                                    display_game_state();
-                                    RandomMove();
-                                }
-                                else {
-                                    while (p.getParent() != null) {
-                                        path.push(p.getColumn());
-                                        path.push(p.getRow());
-                                        p = p.getParent();
-                                    }
-                                    path = path.reverse();
-                                    bluedotmove();
-                                }
-                            }
+    if (!victory_flag && !animation_flag) {
+        for (var j = 0; j < 9; j++) {
+            for (var i = 0; i < 9; i++) {
+                if (j % 2 == 0) {
+                    if ((canvas_x > 10 + (80 * i) && canvas_x < 10 + (80 * (i + 1))) && (canvas_y > 10 + (80 * j) && canvas_y < 10 + (80 * (j + 1)))) {
+                        if (game_state[j][i] != 1 && game_state[j][i] != 2) {
+                            game_controller(j, i);
                         }
                     }
                 }
-            }
-            else {
-                if ((canvas_x > 45 + (80 * i) && canvas_x < 45 + (80 * (i + 1))) && (canvas_y > 10 + (80 * j) && canvas_y < 10 + (80 * (j + 1)))) {
-                    if (game_state[j][i] != 1 && game_state[j][i] != 2) {
-                        if ((blue_dot.row == 8 || blue_dot.row == 0) || (blue_dot.column == 8 || blue_dot.column == 0)) {
-                            if (game_state[blue_dot.row][blue_dot.column] == 2) {
-                                game_state[j][i] = 1;
-                                ClearScreen();
-                                score++;
-                                UpdateScore();
-                            }
-                            game_state[blue_dot.row][blue_dot.column] = 0;
-                            if (!end_animation_flag) {
-                                end_animation_flag = 1;
-                                animation_flag = 1;
-                                var myVar1 = setTimeout(EndAnimation, 500);
-                            }
-                        }
-                        else {
-                            if (victory_flag == 0) {
-                                ClearScreen();
-                                score++;
-                                UpdateScore();
-                                game_state[j][i] = 1;
-                                queue = queue.slice(0, 0);
-                                path = path.slice(0, 0);
-                                var p = MazeSolver(blue_dot.row, blue_dot.column);
-
-                                if (p == null) {
-                                    random_movement_flag = 1;
-                                    display_game_state();
-                                    RandomMove();
-                                }
-                                else {
-                                    while (p.getParent() != null) {
-                                        path.push(p.getColumn());
-                                        path.push(p.getRow());
-                                        p = p.getParent();
-                                    }
-                                    path = path.reverse();
-                                    bluedotmove();
-                                }
-                            }
+                else {
+                    if ((canvas_x > 45 + (80 * i) && canvas_x < 45 + (80 * (i + 1))) && (canvas_y > 10 + (80 * j) && canvas_y < 10 + (80 * (j + 1)))) {
+                        if (game_state[j][i] != 1 && game_state[j][i] != 2) {
+                            game_controller(j, i);
                         }
                     }
                 }
             }
         }
     }
-    if ((canvas_x >= 800 && canvas_x <= 900) && (canvas_y >= 600 && canvas_y <= 700)) {
-        if (animation_flag == 0) {
-            reset_game_state();
-            display_game_state();
-        }
+    if ((canvas_x >= 800 && canvas_x <= 900) && (canvas_y >= 600 && canvas_y <= 700) && (animation_flag == 0)) {
+        reset_game_state();
+        display_game_state();
     }
 }
 
